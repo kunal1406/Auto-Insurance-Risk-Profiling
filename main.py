a003512576dc2f6556bf9b75d589fd63d1ad3a36
kunal <mehtakunalg@gmail.com>
2024-05-01 10:20:41 -0500

    exception, logging and utils added

---
+++ main.py
@@ -0,0 +1,3 @@
+from src.AutoInsurance import logger
+
+logger.info("This is an info message")
--- template.py
+++ template.py
@@ -1,50 +1,57 @@
 import os
 from pathlib import Path
+import logging
 
-project_name = "Auto-Insurance-Risk-Profiling"
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+
+project_name = "AutoInsurance"
+
 
 list_of_files = [
-
-    f"{project_name}/__init__.py",
-    f"{project_name}/components/__init__.py",
-    f"{project_name}/components/data_ingestion.py",  
-    f"{project_name}/components/data_validation.py",
-    f"{project_name}/components/data_transformation.py",
-    f"{project_name}/components/model_trainer.py",
-    f"{project_name}/components/model_evaluation.py",
-    f"{project_name}/components/model_pusher.py",
-    f"{project_name}/configuration/__init__.py",
-    f"{project_name}/constants/__init__.py",
-    f"{project_name}/entity/__init__.py",
-    f"{project_name}/entity/config_entity.py",
-    f"{project_name}/entity/artifact_entity.py",
-    f"{project_name}/exception/__init__.py",
-    f"{project_name}/logger/__init__.py",
-    f"{project_name}/pipline/__init__.py",
-    f"{project_name}/pipline/training_pipeline.py",
-    f"{project_name}/pipline/prediction_pipeline.py",
-    f"{project_name}/utils/__init__.py",
-    f"{project_name}/utils/main_utils.py",
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/entity/config_entity.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "schema.yaml",
+    "main.py",
     "app.py",
-    "requirements.txt",
     "Dockerfile",
-    ".dockerignore",
-    "demo.py",
+    "requirements.txt",
     "setup.py",
-    "config/model.yaml",
-    "config/schema.yaml",
+    "research/trials.ipynb",
+    "templates/index.html",
+    "test.py"
 
 
 ]
 
 
+
+
 for filepath in list_of_files:
     filepath = Path(filepath)
     filedir, filename = os.path.split(filepath)
-    if filedir != "":
+
+
+    if filedir !="":
         os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
+
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath, "w") as f:
             pass
+            logging.info(f"Creating empty file: {filepath}")
+
+
     else:
-        print(f"file is already present at: {filepath}")
+        logging.info(f"{filename} is already exists")
