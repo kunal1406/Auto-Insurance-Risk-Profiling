02ce72dee8d87cbf53744478136d3d28518fb94a
kunal <mehtakunalg@gmail.com>
2024-04-30 22:42:48 -0500

    project structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Auto-Insurance-Risk-Profiling",
+    version="0.0.1",
+    author="Kunal",
+    author_email="mehtakunalg@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
