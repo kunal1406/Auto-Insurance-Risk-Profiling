53a81721cd7ed995bd3983742316374bf36766a7
kunal <mehtakunalg@gmail.com>
2024-05-01 12:18:32 -0500

    data ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from src.AutoInsurance import logger
+from AutoInsurance.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("This is an info message")
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
--- src/AutoInsurance/utils/common.py
+++ src/AutoInsurance/utils/common.py
@@ -1,7 +1,7 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from mlProject import logger
+from AutoInsurance import logger
 import json
 import joblib
 from ensure import ensure_annotations
