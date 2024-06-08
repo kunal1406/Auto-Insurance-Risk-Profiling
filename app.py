370c0dece5be05a9d37e269ae3a5291367806a2c
kunal <mehtakunalg@gmail.com>
2024-06-08 11:47:59 -0500

    pysqlite3 added

--- app.py
+++ app.py
@@ -14,6 +14,10 @@ import os
 from fpdf import FPDF
 import unicodedata
 from AutoInsurance.components.chat_feature import DocumentAnalysis, RiskProfileAnalysis, InsuranceRiskProfileAnalyzer
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 
 user_app = UserAppPipeline()
 risk_profile_model, risk_profiles_df, test = user_app.main()
